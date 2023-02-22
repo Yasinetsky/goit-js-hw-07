fdf9c34a3917268e54f73fa1f1d04bcfc39df501
Oleksii Yasynetskyi <ks7495395@gmail.com>
2023-02-22 13:57:42 +0200

    1/1 from 5

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,13 +3,15 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryContaner = document.querySelector('.gallery');
 const galaryCards = createGalaryCards(galleryItems);
-galleryContaner.insertAdjacentHTML('beforeend', galaryCards)
+
+galleryContaner.insertAdjacentHTML('beforeend', galaryCards);
+
 
 function createGalaryCards(galleryItems) {
     return galleryItems.map(({preview, original, description}) => {
        return `
        <div class="gallery__item">
-         <a class="gallery__link" href="large-image.jpg">
+         <a class="gallery__link" href="${original}">
            <img
              class="gallery__image"
              src="${preview}"
@@ -21,3 +23,44 @@ function createGalaryCards(galleryItems) {
     }).join('');
 }
 
+galleryContaner.addEventListener('click', conteinerGalaryClick);
+
+function conteinerGalaryClick(evt){
+    evt.preventDefault();
+
+    if (!evt.target.classList.contains('gallery__image')) {
+    return;
+   }
+
+    console.log(evt.target.src);
+}
+
+
+// import { galleryItems } from './gallery-items.js';
+// const galleryContainerRef = document.querySelector(".gallery");
+// const imagesMarkup = galleryItems.map(({ preview, original, description }) => {
+//       return `<div class="gallery__item">
+//       <a class="gallery__link" href="${original.value}">
+//         <img
+//           class="gallery__image"
+//           src="${preview}"
+//           data-source="${original}"
+//           alt="${description}"
+//         />
+//       </a>
+//     </div>`;
+//     })
+//     .join("");
+  
+// console.log(imagesMarkup);
+// galleryContainerRef.insertAdjacentHTML("beforeend", imagesMarkup);
+// galleryContainerRef .addEventListener('click', onContainerClick);
+
+// function onContainerClick(e) {
+//       e.preventDefault();
+
+//     if (!e.target.classList.contains('gallery__image')) {
+//         return;
+//     }
+//     console.log(e.target);
+// }

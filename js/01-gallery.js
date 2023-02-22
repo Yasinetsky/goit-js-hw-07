67f7003d9a04fac4e70284a87c03432087c8f73d
Oleksii Yasynetskyi <ks7495395@gmail.com>
2023-02-23 00:05:31 +0200

    4 from 5

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryContaner = document.querySelector('.gallery');
+const galleryContaner = document.querySelector(".gallery");
 const galaryCards = createGalaryCards(galleryItems);
 
-galleryContaner.insertAdjacentHTML('beforeend', galaryCards);
-
+galleryContaner.insertAdjacentHTML("beforeend", galaryCards);
 
 function createGalaryCards(galleryItems) {
-    return galleryItems.map(({preview, original, description}) => {
-       return `
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
        <div class="gallery__item">
          <a class="gallery__link" href="${original}">
            <img
@@ -19,48 +19,25 @@ function createGalaryCards(galleryItems) {
              alt="${description}"/>
          </a>
        </div>
-       `; 
-    }).join('');
+       `;
+    })
+    .join("");
 }
 
-galleryContaner.addEventListener('click', conteinerGalaryClick);
+galleryContaner.addEventListener("click", conteinerGalaryClick);
 
-function conteinerGalaryClick(evt){
-    evt.preventDefault();
+function conteinerGalaryClick(evt) {
+  evt.preventDefault();
 
-    if (!evt.target.classList.contains('gallery__image')) {
+  if (!evt.target.classList.contains("gallery__image")) {
     return;
-   }
+  }
 
-    console.log(evt.target.src);
+  console.log(evt.target.dataset.source);
+
+  const modalImgBig = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+  `);
+
+  modalImgBig.show();
 }
-
-
-// import { galleryItems } from './gallery-items.js';
-// const galleryContainerRef = document.querySelector(".gallery");
-// const imagesMarkup = galleryItems.map(({ preview, original, description }) => {
-//       return `<div class="gallery__item">
-//       <a class="gallery__link" href="${original.value}">
-//         <img
-//           class="gallery__image"
-//           src="${preview}"
-//           data-source="${original}"
-//           alt="${description}"
-//         />
-//       </a>
-//     </div>`;
-//     })
-//     .join("");
-  
-// console.log(imagesMarkup);
-// galleryContainerRef.insertAdjacentHTML("beforeend", imagesMarkup);
-// galleryContainerRef .addEventListener('click', onContainerClick);
-
-// function onContainerClick(e) {
-//       e.preventDefault();
-
-//     if (!e.target.classList.contains('gallery__image')) {
-//         return;
-//     }
-//     console.log(e.target);
-// }

819697a9155810336d2f66f372ad34061d5fae15
Oleksii Yasynetskyi <ks7495395@gmail.com>
2023-02-23 22:26:00 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,30 +1,3 @@
-// import { galleryItems } from "./gallery-items.js";
-// // Change code below this line
-
-// console.log(galleryItems);
-
-// const galleryContaner = document.querySelector(".gallery");
-// const galaryCards = createGalaryCards(galleryItems);
-
-// galleryContaner.insertAdjacentHTML("beforeend", galaryCards);
-
-// function createGalaryCards(galleryItems) {
-//   return galleryItems
-//     .map(({ preview, original, description }) => {
-//       return `
-//        <div class="gallery__item">
-//         <a class="gallery__item"
-//         href="${original}">
-//         <img class="gallery__image"
-//         data-lightbox
-//         src="${preview}"
-//         alt="${description}" /></a>
-//        </div>
-//        `;
-//     })
-//     .join("");
-// }
-
 import { galleryItems } from "./gallery-items.js";
 
 const galleryContainer = document.querySelector(".gallery");

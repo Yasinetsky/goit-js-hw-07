dd6e06cfd0519f197e3d3f4162789a7dfcf87374
Oleksii Yasynetskyi <ks7495395@gmail.com>
2023-02-23 14:35:38 +0200

    2 start

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContaner = document.querySelector(".gallery");
+const galaryCards = createGalaryCards(galleryItems);
+
+galleryContaner.insertAdjacentHTML("beforeend", galaryCards);
+
+function createGalaryCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+       <div class="gallery__item">
+        <a class="gallery__item" 
+        href="${original}">
+        <img class="gallery__image" 
+        src="${preview}"
+        alt="${description}" /></a>
+       </div>
+       `;
+    })
+    .join("");
+}
